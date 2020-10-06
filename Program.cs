fd2174324129e4ed108c5fafcb7ac6960ea92529
fastaki <63346354+fastaki@users.noreply.github.com>
2020-10-07 00:05:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NN_lib;
+
+namespace MainApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path of your image collection");
+            string dir = Console.ReadLine();
+            Console.WriteLine("Enter neural network path");
+            string NN_path = Console.ReadLine();
+
+            My_Lib.ParallelProcess(dir, NN_path);
+        }
+    }
+}
